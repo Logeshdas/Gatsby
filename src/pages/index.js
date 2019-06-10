534611d624eacb5f6bad0e32ff9e323700e63619
Logeshdas <51057319+Logeshdas@users.noreply.github.com>
2019-06-10 12:30:18 +0530

    sorting of date in graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ export default ({data}) => {
 }
 export const query =graphql`
 {
-  allMarkdownRemark {
+  allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
     totalCount
     edges {
       node {
